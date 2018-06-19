cf399203934c5214b79293296f39e4e298be7618
Meahgan <meahgangoeman@gmail.com>
2018-06-19 16:39:08 -0400

    local

--- js/categorySearch.js
+++ js/categorySearch.js
@@ -8,19 +8,16 @@
         <input type="checkbox" ng-model="$ctrl.length" ng-true-value=[46,60]> <p>45-60 MIN</p>
         <input type="checkbox" ng-model="$ctrl.length" ng-true-value=[61,300]> <p>60+ MIN</p>
         <button ng-click="$ctrl.categoryButtons('67,103,101,105')">Art & Design</button>
-        <button ng-click="$ctrl.categoryButtons('104')">Literature</button>
         <button ng-click="$ctrl.categoryButtons('67,93,157')">Business</button>
         <button ng-click="$ctrl.categoryButtons('133')">Comedy</button>
-        <button ng-click="$ctrl.categoryButtons('122')">Society & Culture</button>
+        <button ng-click="$ctrl.categoryButtons('122,104')">Society & Culture</button>
         <button ng-click="$ctrl.categoryButtons('111,112,113,114,115,116,142')">Education</button>
         <button ng-click="$ctrl.categoryButtons('106')">Fashion & Beauty</button>
         <button ng-click="$ctrl.categoryButtons('88,89,90,91')">Fitness & Nutrition</button>
         <button ng-click="$ctrl.categoryButtons('102')">Food</button>
-        <button ng-click="$ctrl.categoryButtons('117')">Government & Organizations</button>
-        <button ng-click="$ctrl.categoryButtons('151')">Local</button>
         <button ng-click="$ctrl.categoryButtons('138,68')">Movies</button>
         <button ng-click="$ctrl.categoryButtons('134')">Music</button>
-        <button ng-click="$ctrl.categoryButtons('99')">News & Politics</button>
+        <button ng-click="$ctrl.categoryButtons('99,117,151')">News & Politics</button>
         <button ng-click="$ctrl.categoryButtons('107,108,108,110')">Natural Sciences</button>
         <button ng-click="$ctrl.categoryButtons('77,141,158,')">Sports & Recreation</button>
         <button ng-click="$ctrl.categoryButtons('112,127,128,130,131,139,140,143,163')">Technology</button>

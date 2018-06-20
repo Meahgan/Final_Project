c58175399c1e06dd660532932595f263828ad333
Meahgan <meahgangoeman@gmail.com>
2018-06-20 14:04:07 -0400

    setup css

--- js/home.js
+++ js/home.js
@@ -1,35 +1,38 @@
 "use strict";
 {
-    let home = {
-        template: `
+  let home = {
+    template: `
+      <div id="home">
         <div class="searchTitle">
           <input ng-model="title" placeholder="Search Titles"/>
           <button class="button" ng-click="$ctrl.searchBar(title)">GO</button>
-        </div>
-        <br>
+          <br>
          <a href=#!/categorySearch><button>Search by Category</button></a>
+        </div>
+      </div>
+        
         `
-        ,
-        //this will call the search title & length function
-        controller: function (podcastService, $location) {
-            let vm = this;
-            vm.podcasts = "";
-            vm.searchBar = function(title){
-              console.log(title);
-              vm.call = podcastService.search(title);
-              vm.call.then(function(){
-                vm.podcasts = podcastService.getPodcast();
+    ,
+    //this will call the search title & length function
+    controller: function (podcastService, $location) {
+      let vm = this;
+      vm.podcasts = "";
+      vm.searchBar = function (title) {
+        console.log(title);
+        vm.call = podcastService.search(title);
+        vm.call.then(function () {
+          vm.podcasts = podcastService.getPodcast();
 
-                console.log(vm.podcasts);
-              });
-            }
+          console.log(vm.podcasts);
+        });
+      }
 
-        }
-    };
+    }
+  };
 
-    home.$inject = ["podcastService", "$location"];
+  home.$inject = ["podcastService", "$location"];
 
-    angular
-        .module("app")
-        .component("home", home);
+  angular
+    .module("app")
+    .component("home", home);
 }
--- js/survey.js
+++ js/survey.js
@@ -2,16 +2,19 @@
 {
   let survey = {
     template: `
+    <div id="survey">
     <p>I Would Use a Podcasts To..</p>
     <input type="checkbox" ng-model="$ctrl.survey" ng-true-value="[67, 93, 94, 95, 147, 149, 187, 111, 127, 125, 107, 125, 163]"><span>Learn</span> <input type="checkbox" ng-model="$ctrl.survey" ng-true-value="[133, 68, 82, 138, 134, 167, 168, 84, 88]"><span>Be Entertained</span>
 
     <p>Right Now, If I Could I Would..</p>
     <input type="checkbox" ng-model="$ctrl.survey2" ng-true-value="[69, 99, 117, 122, 123]"><span>Take a Trip to Europe</span> <input type="checkbox" ng-model="$ctrl.survey2" ng-true-value="[102, 122, 100, 101, 103, 104, 105, 140]"><span>Have a Fun Staycation</span>
+    
     <p>Do You Care More About...</p>
     <input type="checkbox" ng-model="$ctrl.survey3" ng-true-value="[102, 88, 77, 87, 137, 141, 158]"><span>Health</span> <input type="checkbox" ng-model="$ctrl.survey3" ng-true-value="[67, 93, 94, 95, 147, 149, 187, 127, 111]"><span>Money</span>
     <br>
     <br>
-    <input type="button" ng-click="$ctrl.surveyAnswer($ctrl.survey, $ctrl.survey2, $ctrl.survey3)" value="SUBMIT YOUR SURVEY">`,
+    <input type="button" ng-click="$ctrl.surveyAnswer($ctrl.survey, $ctrl.survey2, $ctrl.survey3)" value="SUBMIT YOUR SURVEY">
+    </div>`,
 
     controller: function(podcastService, $location){
       let vm = this;

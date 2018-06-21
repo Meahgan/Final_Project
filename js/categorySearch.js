b88ab4d4ce12898f04fe9480e55eb4f8aa7555ef
tylercschneider <tylercschneider@gmail.com>
2018-06-21 15:18:38 -0400

    cleaned

--- js/categorySearch.js
+++ js/categorySearch.js
@@ -22,90 +22,90 @@
         <div id="categories">
 
             <div class="icons" ng-click="$ctrl.categoryButtons('67,103,101,105')">
-            <img src="art.svg" alt="art"><p>Art & Design</p>
+            <img src="icons/art.svg" alt="art"><p>Art & Design</p>
             </div>
 
             <div class="icons" ng-click="$ctrl.categoryButtons('67,93,157')">
-            <img src="biz.svg" alt="biz"><p>Business</p>
+            <img src="icons/biz.svg" alt="biz"><p>Business</p>
             </div>
 
             <div class="icons" ng-click="$ctrl.categoryButtons('84')">
-            <img src="car.svg" alt="cars">
+            <img src="icons/car.svg" alt="cars">
             <p>Cars</p>
             </div>
 
             <div class="icons" ng-click="$ctrl.categoryButtons('133')">
-            <img src="comedy.svg" alt="comedy">
+            <img src="icons/comedy.svg" alt="comedy">
             <p>Comedy</p>
             </div>
 
             <div class="icons" ng-click="$ctrl.categoryButtons('111')">
-            <img src="education.svg" alt="education">
+            <img src="icons/education.svg" alt="education">
             <p>Education</p>
             </div>
 
             <div class="icons" ng-click="$ctrl.categoryButtons('88,89,90,91')">
-            <img src="fitness.svg" alt="fitness">
+            <img src="icons/fitness.svg" alt="fitness">
             <p>Fitness & Nutrition</p>
             </div>
 
             <div class="icons" ng-click="$ctrl.categoryButtons('102')">
-            <img src="food.svg" alt="food">
+            <img src="icons/food.svg" alt="food">
             <p>Food</p>
             </div>
 
             <div class="icons" ng-click="$ctrl.categoryButtons('148, 150')">
-            <img src="history.svg" alt="history">
+            <img src="icons/history.svg" alt="history">
             <p>History</p>
             </div>
 
             <div class="icons" ng-click="$ctrl.categoryButtons('82, 88')">
-            <img src="hobbies.svg" alt="hobbies">
+            <img src="icons/hobbies.svg" alt="hobbies">
             <p>Hobbies</p>
             </div>
 
             <div class="icons" ng-click="$ctrl.categoryButtons('138,68')">
-            <img src="movieCam.svg" alt="movie">
+            <img src="icons/movieCam.svg" alt="movie">
             <p>Movies</p>
             </div>
 
             <div class="icons" ng-click="$ctrl.categoryButtons('134')">
-            <img src="music-note.svg" alt="music">
+            <img src="icons/music-note.svg" alt="music">
             <p>Music</p>
             </div>
 
             <div class="icons" ng-click="$ctrl.categoryButtons('123')">
-            <img src="travel.svg" alt="travel">
+            <img src="icons/travel.svg" alt="travel">
             <p>Places & Travel</p>
             </div>
 
             <div class="icons" ng-click="$ctrl.categoryButtons('99,117,151')">
-            <img src="politics.svg" alt="politics">
+            <img src="icons/politics.svg" alt="politics">
             <p>News & Politics</p>
             </div>
 
             <div class="icons" ng-click="$ctrl.categoryButtons('107,108,108,110')">
-            <img src="science.svg" alt="science">
+            <img src="icons/science.svg" alt="science">
             <p>Science</p>
             </div>
 
             <div class="icons" ng-click="$ctrl.categoryButtons('69,70,71,72,73,74,75,76')">
-            <img src="religion.svg" alt="religion">
+            <img src="icons/religion.svg" alt="religion">
             <p>Religion</p>
             </div>
 
             <div class="icons" ng-click="$ctrl.categoryButtons('122,104')">
-            <img src="society.svg" alt="society">
+            <img src="icons/society.svg" alt="society">
             <p>Society & Culture</p>
             </div>
 
             <div class="icons" ng-click="$ctrl.categoryButtons('77,141,158,')">
-            <img src="sports.svg" alt="sports">
+            <img src="icons/sports.svg" alt="sports">
             <p>Sports & Recreation</p>
             </div>
 
             <div class="icons" ng-click="$ctrl.categoryButtons('112,127,128,130,131,139,140,143,163')">
-            <img src="tech.svg" alt="tech">
+            <img src="icons/tech.svg" alt="tech">
             <p>Technology</p>
             </div>
 

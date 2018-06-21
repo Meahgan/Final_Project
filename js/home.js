03a42ae9a248717b349e2e1e45f827710a5e2cf5
tomas <perezgantomas@gmail.com>
2018-06-21 16:44:33 -0400

    my commit

--- js/home.js
+++ js/home.js
@@ -13,9 +13,9 @@
       <div class="searchTitle" ng-hide="$ctrl.move">
         <h1 class="titleBars">Cool Name</h1>
         <span><input class="searchBar" ng-model="title" placeholder="Search Titles"/>
-        <button class="goBtn" ng-click="$ctrl.searchBar(title)">GO</button></span>
-        <a href="#!/survey"><button class="searchBtns">Survey</button></a>
-        <a href=#!/categorySearch><button class="searchBtns">Search by Category</button></a>
+        <button class="goBtn" ng-click="$ctrl.searchBar(title)" ng-click="$ctrl.wow()">GO</button></span>
+        <a href="#!/survey"><button class="searchBtns" ng-click="$ctrl.wow()">Survey</button></a>
+        <a href=#!/categorySearch><button class="searchBtns" ng-click="$ctrl.wow()">Search by Category</button></a>
       </div>
     </div>
         `

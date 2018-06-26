bfe0110e3354a5b16565e3c0bb34d8befc090fe1
tylercschneider <tylercschneider@gmail.com>
2018-06-26 17:04:20 -0400

    icon crazy

--- js/podcastService.js
+++ js/podcastService.js
@@ -9,7 +9,21 @@
 
         const setPodcast = function (newPodcast) {
             for(let i=0; i<newPodcast.length; i++){
-                podcasts.unshift(newPodcast[i]);
+                let temp = newPodcast[i];
+                let entry = {
+                    title_original: temp.title_original,
+                    audio: temp.audio,
+                    description_original: temp.description_original,
+                    catIcons: []
+                };
+                for(let j=0; j<temp.genres.length; j++){
+                    let iconEntry = iconKey[temp.genres[i]];
+                    if(!!(entry.catIcons.indexOf(iconEntry)+1)){
+                        entry.catIcons.push(iconEntry);
+                    }
+                }
+                console.log(entry);
+                podcasts.unshift(entry);
             }
 
         }
@@ -83,6 +97,52 @@
             $location.path("/results");
             return podcasts;
         });
+      };
+
+      let iconKey = {
+          "67": 'icons/biz.svg',
+          "93": 'icons/biz.svg',
+          "94": 'icons/biz.svg',
+          "95": 'icons/biz.svg',
+          "147": 'icons/biz.svg',
+          "149": 'icons/biz.svg',
+          "187": 'icons/biz.svg',
+          "127": 'icons/tech.svg',
+          "111": 'icons/education.svg',
+          "102": 'icons/food.svg',
+          "122": 'icons/society.svg',
+          "100": 'icons/art.svg',
+          "101": 'icons/art.svg',
+          "103": 'icons/art.svg',
+          "104": 'icons/art.svg',
+          "105": 'icons/art.svg',
+          "140": 'icons/art.svg',
+          "133": 'icons/comedy.svg',
+          "68": 'icons/movieCam.svg',
+          "82": 'icons/movieCam.svg',
+          "138": 'icons/movieCam.svg',
+          "134": 'icons/music-note.svg',
+          "167": 'icons/music-note.svg',
+          "168": 'icons/music-note.svg',
+          "84": 'icons/car.svg',
+          "82": 'icons/hobbies.svg',
+           "88": 'icons/hobbies.svg',
+           "88": 'icons/fitness.svg',
+            "77": 'icons/sports.svg',
+            "87": 'icons/sports.svg',
+            "137": 'icons/sports.svg',
+            "141": 'icons/sports.svg',
+            "158": 'icons/sports.svg',
+            "125": 'icons/history.svg',
+            "125": 'icons/science.svg',
+             "107": 'icons/science.svg',
+              "163": 'icons/science.svg',
+              "69": 'icons/religion.svg',
+              "99": 'icons/politics.svg',
+              "117": 'icons/politics.svg',
+              "122": 'icons/travel.svg',
+              "123": 'icons/travel.svg'
+
       };
             return {
                 getPodcast,

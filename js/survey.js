da63005f6e8cc64dec246b4d689eedb914ed745e
kalebjohnson98 <kaleb.j.business@gmail.com>
2018-06-26 18:20:55 -0400

    all home pages looking fresh

--- js/survey.js
+++ js/survey.js
@@ -8,13 +8,16 @@
       vm.survey = "";
       vm.survey2 = "";
       vm.survey3 = "";
+      vm.length = "";
       vm.move = false;
       vm.surveyAnswer = function(survey, survey2, survey3){
         vm.move = true;
         console.log(survey);
         console.log(vm.survey);
+        let min = vm.length[0];
+        let max = vm.length[1];
         let surveys = survey + survey2 + survey3;
-        vm.surveyClick = podcastService.surveySearch(surveys);
+        vm.surveyClick = podcastService.surveySearch(surveys, min, max);
         vm.surveyClick.then(function () {
           console.log(surveys);
           $location.path("/results");
